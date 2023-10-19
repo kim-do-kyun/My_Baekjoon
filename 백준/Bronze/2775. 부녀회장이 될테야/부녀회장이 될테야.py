9f2427658ec4d15e37ad07ac217b8767304048f5
kim-do-kyun <70315428+kim-do-kyun@users.noreply.github.com>
2023-10-20 00:33:33 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for i in range(t):
+    k = int(input())
+    n = int(input())
+    peoples = [i for i in range(1, n+1)]
+    
+    for x in range(k):
+        for y in range(1, n):
+            peoples[y] += peoples[y-1]
+    print(peoples[-1])
