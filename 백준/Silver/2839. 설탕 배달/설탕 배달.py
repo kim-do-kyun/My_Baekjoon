4c93adee213f2dd5f084fffd8fc2ee6b7dbf447d
kim-do-kyun <70315428+kim-do-kyun@users.noreply.github.com>
2024-01-08 23:47:23 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+sugar = int(input())
+bag = 0
+while sugar >= 0:
+    if sugar % 5 == 0:
+        bag += sugar // 5
+        print(bag)
+        break
+    sugar -= 3
+    bag += 1
+else:
+    print(-1)
