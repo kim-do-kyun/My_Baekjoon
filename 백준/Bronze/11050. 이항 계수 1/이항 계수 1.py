733be89c48219bd47c04a8eb8a0cca404bc7e7fd
kim-do-kyun <70315428+kim-do-kyun@users.noreply.github.com>
2024-01-16 18:23:38 +0900

    [Bronze I] Title: 이항 계수 1, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+def fact(n):
+    if n == 0:
+        return 1
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+n, k = map(int, input().split())
+print(fact(n) // (fact(k) * fact(n - k)))
