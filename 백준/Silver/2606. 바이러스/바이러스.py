d4f792fb3b1d0127c04a1832b68b4e916fe40302
kim-do-kyun <70315428+kim-do-kyun@users.noreply.github.com>
2024-01-20 14:17:21 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+connected = int(input())
+
+graph = [[] for _ in range(n+1)]
+
+for i in range(connected):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+visited = [False]*(n+1)
+count = -1
+
+def DFS(v):
+    visited[v] = True
+    global count
+    count += 1
+    for i in graph[v]:
+        if not visited[i]:
+            DFS(i)
+            
+DFS(1)
+print(count)
