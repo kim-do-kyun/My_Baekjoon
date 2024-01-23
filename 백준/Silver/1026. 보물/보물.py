455d309611b3e63f82ec588bbf757d54087daccf
kim-do-kyun <70315428+kim-do-kyun@users.noreply.github.com>
2024-01-23 22:14:22 +0900

    [Silver IV] Title: 보물, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,10 @@
+n = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+A.sort(reverse = True)
+B.sort()
+result = 0
+for i in range(n):
+    result += A[i]*B[i]
+
+print(result)
