5872705a31e8a456505f95a46ddb706f3d47ec93
kim-do-kyun <70315428+kim-do-kyun@users.noreply.github.com>
2024-01-28 17:11:27 +0900

    [Silver III] Title: 모든 순열, Time: 204 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+temp = []
+
+def dfs():
+    if len(temp) == n:
+        print(*temp)
+        return
+    for i in range(1, n+1):
+        if i not in temp:
+            temp.append(i)
+            dfs()
+            temp.pop()
+dfs()
