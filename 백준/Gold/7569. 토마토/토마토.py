87997f911d05285086c598261dc4ab86ac63b5da
kim-do-kyun <70315428+kim-do-kyun@users.noreply.github.com>
2024-02-07 15:27:33 +0900

    [Gold V] Title: 토마토, Time: 1864 ms, Memory: 57552 KB -BaekjoonHub

---
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -0,0 +1,50 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+m, n, h = map(int, input().split())
+tomato_info = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
+visited = [[[False]*m for _ in range(n)] for _ in range(h)]
+
+queue = deque()
+
+dx = [-1, 1, 0, 0, 0, 0]
+dy = [0, 0, -1, 1, 0, 0]
+dz = [0, 0, 0, 0, -1, 1]
+
+result = 0
+
+def bfs():
+    while queue:
+        x, y, z = queue.popleft()
+        
+        for i in range(6):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            nz = z + dz[i]
+            
+            if nx < 0 or nx >= h or ny < 0 or ny >= n or nz < 0 or nz >= m:
+                continue
+                
+            if tomato_info[nx][ny][nz] == 0 and visited[nx][ny][nz] == False:
+                queue.append((nx, ny, nz))
+                tomato_info[nx][ny][nz] = tomato_info[x][y][z] + 1
+                visited[nx][ny][nz] = True
+                
+for a in range(h):
+    for b in range(n):
+        for c in range(m):
+            if tomato_info[a][b][c] == 1 and visited[a][b][c] == 0:
+                queue.append((a, b, c))
+                visited[a][b][c] = True
+                
+bfs()
+
+for a in tomato_info:
+    for b in a:
+        for c in b:
+            if c == 0:
+                print(-1)
+                exit(0)
+        result = max(result, max(b))
+        
+print(result-1)
